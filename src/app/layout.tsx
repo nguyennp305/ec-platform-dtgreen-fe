77700a31616465abc8b94164b4b0ca014c19acf0
nguyennp305 <20021406@vnu.edu.vn>
2024-07-03 09:16:23 +0700

    add status head in SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Layout/Footer";
 import Header from "@/components/Layout/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
+import Head from 'next/head';
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 
@@ -21,7 +22,18 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      {/* <head /> */}
+      <Head>
+        <title>Trang chủ</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Mô tả của trang chủ của bạn" />
+        <link rel="canonical" href="https://example.com" />
+        <meta name="keywords" content="từ khóa, tối ưu hóa SEO" />
+        <meta name="author" content="Tên tác giả" />
+        <meta name="robots" content="index, follow" />
+        {/* Thêm các thẻ meta khác tương ứng với nhu cầu của bạn */}
+      </Head>
 
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className} overflow-y-scroll custom-scroll`}>
         <UserProvider>
